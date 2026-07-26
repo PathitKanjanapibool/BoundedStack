d27bfb8a89ed4481daceb74cb79341dea75e46b0
Phromphun <phromphun.w@ku.th>
2026-07-26 11:00:37 +0700

    version0.1.1
    Add RI for element and capacity

--- BoundedStack.java
+++ BoundedStack.java
@@ -5,10 +5,19 @@ public class BoundedStack {
     private final List<String> element;
     private final int capacity;
 
-    //AF(element,capacity) = รายการ String โดยเรียงตามลำดับใน element ,ข้อมูลขนาดของ Stack
-    //RI
-    //-
-    //-
+    //AF(element,capacity) = รายการ String โดยเรียงตามลำดับใน element ,ข้อมูลขนาดของ Stack เป็นจำนวนเต็ม
+    //RI (element)
+    // element != null
+    // ไม่มีสมาชิกตัวใดเป็น null
+    // ไม่มี String ที่ว่าง
+    // String ห้ามซ้ำ
+    // มีข้อมูล String ไม่เกิน capacity
+    //RI (capacity)
+    // capacity != null
+    // capacity != ""
+    // capacity ต้องไม่ติดลบ
+    // ห้ามเว้นวรรค
+    // ห้ามเป็นทศนิยม
 
 
     /**
